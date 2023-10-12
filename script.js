546c1d38dbc947611a0fac21ac9d13f738155085
alenabell mays <alenabellmays@gmail.com>
2023-10-12 13:36:48 -0700

    edited html and script file

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-let data = [
-  "France",
-  "Germany",
-  "England",
-  "Spain",
-  "Belgium",
-  "Italy",
-  "Portugal",
-  "Irland",
-  "Luxembourg",
-];
+var app = new (function () {
+  this.el = document.getElementById("countries");
+  this.countries = [
+    "France",
+    "Germany",
+    "England",
+    "spain",
+    "Belgium",
+    "Italy",
+    "Portugal",
+    "Ireland",
+    "Luxemburg",
+  ];
+
+  this.Count = function (data) {
+    var el = document.getElementById("counter");
+    var name = "country";
+
+    if (data) {
+      if (data > 1) {
+        name = "countries";
+      }
+      el.innerHTML = data + " " + name;
+    } else {
+      el.innerHTML = "No " + name;
+    }
+  };
+
+  this.FetchAll = function () {
+    var data = "";
+
+    if (this.countries.length > 0) {
+      for (i = 0; i < this.countries.length; i++) {
+        data += "<tr>";
+        data += "<td>" + this.countries[i] + "</td>";
+        data += '<td><button onclick="app.Edit(' + i + ')"</button></td>';
+        data += '<td><button onclick="app.Delete(' + i + ')"</button></td>';
+        data += "</tr>";
+      }
+    }
+  };
+})();
